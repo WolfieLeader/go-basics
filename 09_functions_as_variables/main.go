aacfc649abecd6079e9de00c1f1d775891921bd9
WolfieLeader <koren2318@gmail.com>
2025-06-19 17:18:43 +0300

    functions as variables added

--- 09_functions_as_variables/main.go
+++ 09_functions_as_variables/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func fibonacciWithLocalVar() func() int {
+	// This is a closure that captures the local variables
+	a, b := 0, 1
+
+	// It returns a function that generates the next Fibonacci number
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
 func main() {
 	// This is a variable that holds an anonymous function
 	someFunc := func(x int) int {
@@ -9,4 +20,12 @@ func main() {
 	}
 
 	fmt.Println(someFunc(5)) // Output: 25
+	fibFunc := fibonacciWithLocalVar()
+	for range 10 {
+		fmt.Println(fibFunc()) // Output: Fibonacci numbers
+	}
+
+	func() {
+		fmt.Println("This is an anonymous function that runs immediately")
+	}()
 }
