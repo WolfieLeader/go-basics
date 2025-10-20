7e1609c27c0c371f8686653e0b5bd6e9415c22eb
WolfieLeader <koren2318@gmail.com>
2025-10-20 15:56:38 +0300

    encoding added

---
+++ 19_data_manipulation/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Base64 Simple Example:")
+	base64SimpleExample()
+	fmt.Println()
+
+	fmt.Println("Base64 Encoder Example:")
+	base64EncoderExample()
+	fmt.Println()
+
+	fmt.Println("Hex Simple Example:")
+	hexSimpleExample()
+	fmt.Println()
+
+	fmt.Println("Hex Encoder Example:")
+	hexEncoderExample()
+	fmt.Println()
+}
